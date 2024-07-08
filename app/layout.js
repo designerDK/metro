be3ff7c6de8bc8200ead6927011093a2a95ebf78
designerDK <tupacalypse@naver.com>
2024-07-08 22:23:36 +0900

    v1.0.0

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,15 @@ export default function RootLayout({ children }) {
       <meta name="google-site-verification" content="ohrGnA7RsHT30X375ixKOmz7HJIZHM3ZBCPU3Y2ootg" />
       <meta name="naver-site-verification" content="c683e5352a3b6b828107057ea5e1f2d76596ba3c" />
 
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-4HQ7PVQH4J"></script>
+      <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+
+      gtag('config', 'G-4HQ7PVQH4J');
+      </script>
+
       <meta property="og:title" content="서울 지하철역 이름 뜻과 유래"></meta>
       <meta property="og:description" 
       content="서울 지하철역들의 유래와 각 역명의 의미를 검색해 볼 수 있는 사이트입니다."></meta>
