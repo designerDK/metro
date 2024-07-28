3091cc37df1daede5c0a72a28462040fa0443c02
designerDK <tupacalypse@naver.com>
2024-07-28 19:59:51 +0900

    v1.0.1

--- app/page.js
+++ app/page.js
@@ -109,6 +109,11 @@ export default function Home() {
       </section>
 
       <div className="divider"></div>
+      <ins class="kakao_ad_area" style="display:none;"
+      data-ad-unit = "DAN-yhIQZ5JxCeh8ffyS"
+      data-ad-width = "320"
+      data-ad-height = "50"></ins>
+<script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
 
       <section className="list-section">
         <div className="list-containers"> 
